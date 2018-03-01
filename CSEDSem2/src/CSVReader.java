56115b64a6ddbb82486b70cfcc66209245ee8900
Alex Bourne <36666272+ajbourne98@users.noreply.github.com>
2018-03-01 10:39:22 +0000

    Update CSVReader.java
    Added comments.

--- CSEDSem2/src/CSVReader.java
+++ CSEDSem2/src/CSVReader.java
@@ -1,31 +1,58 @@
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
+import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Scanner;
+
+/**
+ * A class that reads from and writes to .csv files.
+ * @author ajb258
+ * @version 1.0
+ * @release N/A
+ */
 
 public class CSVReader
 {
-	String csvFile = "C:/Users/alexb/Documents/Uni/CM10251/csv.csv";
+	String csvFile = "example.csv";
 	String line = "";
 	String csvSplit = ",";
+	
+	/**
+	 * Constructor. Calls the read() method.
+	 */
 	
 	public CSVReader()
 	{
 		read();
 	}
 	
+	/**
+	 * Method that reads from a .csv file, and displays the contents in the console.
+	 */
+	
 	public void read()
 	{
 		try
 		{
 			BufferedReader br = new BufferedReader(new FileReader(csvFile));
-			
+			int iteration = 0;
 			while ((line = br.readLine()) != null)
 			{
-				String[] fields = line.split(csvSplit);
-				System.out.println("ID: " + fields[0]);
-				System.out.println("Name: " + fields[1]);
-				System.out.println("Gender: " + fields[2]);
+				// If statement prevents headers of csv file from being displayed.
+				if (iteration == 0)
+				{
+					iteration++;
+				}
+				else
+				{
+					String[] fields = line.split(csvSplit);
+					System.out.println("ID: " + fields[0]);
+					System.out.println("Name: " + fields[1]);
+					System.out.println("Age: " + fields[2]);
+					System.out.println("Gender: " + fields[3] + "\n");
+				}	
 			}
 			
 			br.close();
