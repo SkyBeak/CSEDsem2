9ae129de031ab2dadd5da55ae8fa98188bb384e1
Clifford <Clifford@DESKTOP-83SHHUP>
2018-03-20 11:15:36 +0000

    added the ability to write to the entries csv and changed the text-based ui

--- src/CSVWriter.java
+++ src/CSVWriter.java
@@ -1,12 +1,26 @@
 import java.io.*;
+import java.time.LocalDateTime;
 
 public class CSVWriter	{
+	
+	FileWriter entryWriter;
 	/*TODO: *implement a method to add an entry to the entries csv
 	 * 		*implement a method to add a new user to the users csv*/
 	public CSVWriter(){
 		
 	}
 	
-	
+	public void addEntry(int type,int userID, int calories){
+		try {
+			entryWriter = new FileWriter(new File("src/entries.csv"),true);
+			String date = LocalDateTime.now().toString().substring(0, 10);
+			entryWriter.write("\n"+type+","+userID+","+calories+","+date+",");
+			System.out.println("entry written to file");
+			entryWriter.close();
+		} catch (IOException e) {
+			System.err.println("Unable to write to file");
+			System.exit(1);
+		}
+	}
 	
 }
--- src/UI.java
+++ src/UI.java
@@ -2,9 +2,11 @@ import java.io.*;
 import java.util.ArrayList;
 
 
+
 public class UI	{
 	
 	CSVReader entryReader;
+	CSVWriter entryWriter;
 	
 	public static void main(String[] args){
 		UI textInterface = new UI();
@@ -13,10 +15,38 @@ public class UI	{
 	 * constructor, currently just sets up an entry reader and runs any test methods.
 	 */
 	public UI(){
-
+		
 		entryReader  = new CSVReader();
-		//printAllEntries();
-		printSearchResults("xxxx", "1");
+		entryWriter = new CSVWriter();
+		
+		System.out.println("Please input either breakfast, lunch or dinner using the commands:\n\"addBreakfast\", \"addLunch\", \"addDinner\"\nOr view entries using the \"viewEntries\" command.");
+		
+		BufferedReader userInput = new BufferedReader(new InputStreamReader(System.in));
+		String userCommand;
+		try{
+			while((userCommand = userInput.readLine().toUpperCase())!=null){
+				switch(userCommand){
+
+				case "ADDBREAKFAST": addEntry(userInput,1);
+				break;
+
+				case "ADDLUNCH": addEntry(userInput,2);
+				break;
+
+				case "ADDDINNER": addEntry(userInput,3);
+				break;
+				
+				case "VIEWENTRIES": printAllEntries();
+				
+				case "EXIT": System.exit(0);
+				
+				default: System.out.println("Please input either breakfast, lunch or dinner using the commands:\n\"addBreakfast\", \"addLunch\", \"addDinner\"\nOr view entries using the \"viewEntries\" command.");
+				}
+			}
+		}catch(IOException e){
+
+		}
+		
 		
 	}
 	
@@ -44,6 +74,39 @@ public class UI	{
 	private ArrayList<Entry> searchFor(String field, String value){
 		ArrayList<Entry> found = entryReader.SearchFor(field.toUpperCase(), value);
 		return found;
+	}
+	
+	private void addEntry(BufferedReader br, int type){
+		System.out.println("Please input the number of calories consumed");
+		String calories = "";
+		try{
+			calories = br.readLine();
+		}catch(IOException e){
+			System.err.println("Error getting user input");
+			System.exit(1);
+		}
+		
+		if(isNumeric(calories)){
+			int cal = Integer.parseInt(calories);
+			entryWriter.addEntry(type,1,cal);
+		}else{
+			addEntry(br,type);
+		}
+	}
+	
+	/**
+	 * 
+	 * @param s: the string to be tested
+	 * @return whether the string passed in is a sequence of numerical digits
+	 */
+	private boolean isNumeric(String s){
+		for(int i = 0; i<s.length();i++){
+			Character current = s.charAt(i);
+			if(!Character.isDigit(current)){
+				return false;
+			}
+		}
+		return true;
 	}
 	
 	
