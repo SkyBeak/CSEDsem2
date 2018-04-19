af831525e5940553e0feca8611b3a083ff311142
benfisherburgess <36888499+benfisherburgess@users.noreply.github.com>
2018-04-19 12:52:40 +0100

    Update CSVWriter.java

--- src/CSVWriter.java
+++ src/CSVWriter.java
@@ -5,41 +5,41 @@ import java.util.Date;
 import java.text.SimpleDateFormat;
 
 public class CSVWriter	{
-	
-	FileWriter entryWriter;
-	FileWriter userWriter;
-	
-	public CSVWriter(){
-		
-	}
-	
-	public void addEntry(int type, int calories){
-		try {
-			entryWriter = new FileWriter(new File("src/entries.csv"),true);
-			SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
-			Calendar c = Calendar.getInstance();
-			String date = sdf.format(c.getTime());
-			entryWriter.write("\n"+type+","+calories+","+date+",");
-			System.out.println("entry written to file");
-			entryWriter.close();
-		} catch (IOException e) {
-			System.err.println("Unable to write to file");
-			System.exit(1);
-		}
-	}
-	
-	/* adds user to user.csv file
-	 * Now not really needed I guess?*/
-    	public void addUser(int height, int age, String sex){
-        	try{
-            		userWriter = new FileWriter(new File("src/users.csv"), true);
-            		userWriter.write("\n"+height+","+age+","+sex+",");
-            		System.out.println("New user written to file");
-            		userWriter.close();
-        	} catch (IOException e) {
-            		System.err.println("Unable to write to file");
-            		System.exit(1);
+
+    FileWriter entryWriter;
+    FileWriter userWriter;
+
+    public CSVWriter(){
+
+    }
+
+    public void addEntry(int type, int calories){
+        try {
+            entryWriter = new FileWriter(new File("src/entries.csv"),true);
+            SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/YYYY");
+            Calendar c = Calendar.getInstance();
+            String date = sdf.format(c.getTime());
+            entryWriter.write("\n"+type+","+calories+","+date+",");
+            System.out.println("entry written to file");
+            entryWriter.close();
+        } catch (IOException e) {
+            System.err.println("Unable to write to file");
+            System.exit(1);
         }
     }
-	
+
+    /* adds user to user.csv file
+     * Now not really needed I guess?*/
+    public void addUser(String name, int height, int age, String sex, int weight, int targetWeight, String targetTime){
+        try{
+            userWriter = new FileWriter(new File("src/users.csv"), true);
+            userWriter.write("\n"+ name + "," + sex + "," + height + "," + weight + "," + age + "," +targetWeight + "," + targetTime);
+            System.out.println("New user written to file");
+            userWriter.close();
+        } catch (IOException e) {
+            System.err.println("Unable to write to file");
+            System.exit(1);
+        }
+    }
+
 }
