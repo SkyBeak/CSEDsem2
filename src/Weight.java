1f8c452dadface52a0547ade42c2353937a582cc
benfisherburgess <36888499+benfisherburgess@users.noreply.github.com>
2018-04-24 13:12:03 +0100

    Create Weight.java

---
+++ src/Weight.java
@@ -0,0 +1,28 @@
+public class Weight {
+
+    private int weight;
+    private String date;
+
+    public Weight(int weight, String date){
+        this.weight = weight;
+        this.date = date;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+
+
+}
